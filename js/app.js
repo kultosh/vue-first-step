afcfa653b37aaad25ffec7cefdfeb576331e3182
kultosh <kulgole90@gmail.com>
2020-05-05 23:23:06 +0545

    No Change

--- js/app.js
+++ js/app.js
@@ -1,14 +1,7 @@
 new Vue ({
 	el: '#first-vue-app',
 	data: {
-		age: 18,
-	},
-	methods: {
-		logName:function(){
-			console.log("You Entered the name!")
-		},
-		logJob:function(){
-			console.log("You Entered the job");
-		}
+		name: '',
+		job: '',
 	}
 })
