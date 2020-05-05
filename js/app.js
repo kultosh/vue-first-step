624b9ee19a5e448c20486b6d68905c0b57ab24d4
kultosh <kulgole90@gmail.com>
2020-05-05 13:36:26 +0545

    Created mousemove Event where the co-ordinate of the canvase is shown as per the mouse move & parameter event in the function need not to be pass through index as it is known in the event

--- js/app.js
+++ js/app.js
@@ -2,5 +2,13 @@ new Vue ({
 	el: '#first-vue-app',
 	data: {
 		age: 18,
+		x: 0,
+		y:0,
 	},
+	methods: {
+		updateXY:function(event){
+			this.x = event.offsetX;
+			this.y = event.offsetY;
+		}
+	}
 })
