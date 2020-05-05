b08e8815f77a138dd065f75a42a3cf1c9a0ffc6d
kultosh <kulgole90@gmail.com>
2020-05-05 12:55:51 +0545

    Simple Click Event to increase and decrease age using v-on:click

--- js/app.js
+++ js/app.js
@@ -1,15 +1,6 @@
 new Vue ({
 	el: '#first-vue-app',
 	data: {
-		firstname: 'Santosh',
-		lastname: 'Tamang',
-		job: 'Developer',
-		website: 'https://vuejs.org/',
-		websiteTag: '<a href="https://vuejs.org/">HTML Tag Data Binding</a>'
+		age: 18,
 	},
-	methods: {
-		greet:function(time){
-			return 'Good ' + time + ' ' + this.firstname;
-		}
-	}
 })
