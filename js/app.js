d6b1caf308997c869cebcf3d855a30dcb50ab423
kultosh <kulgole90@gmail.com>
2020-05-06 00:19:12 +0545

    Computed Properties instead of methods for the event to take place for the particular one

--- js/app.js
+++ js/app.js
@@ -1,7 +1,28 @@
 new Vue ({
 	el: '#first-vue-app',
 	data: {
-		name: '',
-		job: '',
+		age: 20,
+		A: 0,
+		B: 0,
+	},
+	methods: {
+		// addA:function(){
+		// 	console.log('addA');
+		// 	return this.age + this.A;
+		// },
+		// addB:function() {
+		// 	console.log('addB');
+		// 	return this.age + this.B;
+		// }
+	},
+	computed: {
+		addA:function(){
+			console.log('addA');
+			return this.age + this.A;
+		},
+		addB:function() {
+			console.log('addB');
+			return this.age + this.B;
+		}
 	}
 })
