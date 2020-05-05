f76046acdd74297ceee2805e6354fa8cddba0b18
kultosh <kulgole90@gmail.com>
2020-05-05 12:33:18 +0545

    Data binding using v-bind: or : & html tag binding using v-html

--- js/app.js
+++ js/app.js
@@ -4,6 +4,8 @@ new Vue ({
 		firstname: 'Santosh',
 		lastname: 'Tamang',
 		job: 'Developer',
+		website: 'https://vuejs.org/',
+		websiteTag: '<a href="https://vuejs.org/">HTML Tag Data Binding</a>'
 	},
 	methods: {
 		greet:function(time){
