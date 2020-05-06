e45c47360060de947c8c7adf5a9f95da43ff14e6
kultosh <kulgole90@gmail.com>
2020-05-06 18:23:53 +0545

    Created dynamic classes using data binding & click event

--- js/app.js
+++ js/app.js
@@ -1,28 +1,18 @@
 new Vue ({
 	el: '#first-vue-app',
 	data: {
-		age: 20,
-		A: 0,
-		B: 0,
+		available: true,
+		nearby: false,
 	},
 	methods: {
-		// addA:function(){
-		// 	console.log('addA');
-		// 	return this.age + this.A;
-		// },
-		// addB:function() {
-		// 	console.log('addB');
-		// 	return this.age + this.B;
-		// }
+
 	},
 	computed: {
-		addA:function(){
-			console.log('addA');
-			return this.age + this.A;
-		},
-		addB:function() {
-			console.log('addB');
-			return this.age + this.B;
+		compClasses:function(){
+			return {
+				 available: this.available,
+				 nearby: this.nearby,
+			 };
 		}
 	}
 })
