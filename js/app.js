2e619788d4c24d38ec35c1ec22a604738c037e9b
kultosh <kulgole90@gmail.com>
2020-05-07 12:03:08 +0545

    Created loop using v-for also include key and value

--- js/app.js
+++ js/app.js
@@ -1,8 +1,17 @@
 new Vue ({
 	el: '#first-vue-app',
 	data: {
-		error: true,
-		success: false,
+		characters: ['Captain America','Iron Man','Thor','Spider Man'],
+		actors:[
+			{
+				name:'Chris Evan',
+				age: 45,
+			},
+			{
+				name: 'Robert Downy JR',
+				age: 52,
+			},
+		]
 	},
 	methods: {
 
